4d86a8770320ed988636c92510863b92a36e258b
cristianbarallobrerua <cristianbarallobrerua@gmail.com>
2020-10-30 10:48:50 +0100

    ejercicio procesos terminado

--- Ejercicio8SYP/src/base/Principal.java
+++ Ejercicio8SYP/src/base/Principal.java
@@ -1,18 +1,26 @@
 package base;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Principal {
 
 	static final int NUM_PROCESOS = 5;
 	static final String PREFIJO_FICHEROS = "fichero";
 
+	/*
+	 * Método para lanzar un proceso en el que sumaremos las vocales de un fichero
+	 */
 	public static void lanzarSumadorVocales(String nombreFicheroEntrada, String letra, String nombreFicheroResultado)
 			throws IOException {
 		String clase = "base.SumadorVocales";
-		File directorioSumadorVocales = new File("C:\\Users\\Usuario\\proyectosEclipseClase1\\Ejercicio8SYP\\bin");
+		
+		File directorioSumadorVocales = new File(
+				"D:\\ProyectosEclipseSegundo\\proyectosEclipseClase\\Ejercicio8SYP\\bin");
 		try {
 			ProcessBuilder pb = new ProcessBuilder("java", clase, nombreFicheroEntrada, letra);
 			pb.directory(directorioSumadorVocales);
@@ -23,17 +31,52 @@ public class Principal {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+	/* Método para recoger el entero de la primera línea de un fichero de texto */
+	public static int getResultadoFichero(String nombreFichero) {
+		int suma = 0;
+		try {
+			FileInputStream fichero = new FileInputStream(nombreFichero);
+			InputStreamReader fir = new InputStreamReader(fichero);
+			BufferedReader br = new BufferedReader(fir);
+			String linea = br.readLine();
+			suma = Integer.parseInt(linea);
+			return suma;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo abrir " + nombreFichero);
+		} catch (IOException e) {
+			System.out.println("No hay nada en " + nombreFichero);
+		}
+		return suma;
+	}
+
+	/* Método para pasar por los distintos ficheros que se vamos a sumar */
+	public static int getSumaTotal(int numFicheros) {
+		int sumaTotal = 0;
+		for (int i = 0; i < NUM_PROCESOS; i++) {
+			sumaTotal += getResultadoFichero(PREFIJO_FICHEROS + String.valueOf(i + 1));
+		}
+		return sumaTotal;
+	}
+
+	/*
+	 * Método main donde pasamos como argumento el fichero que queremos analizar y
+	 * determinamos el nº de procesos a lanzar y finalmente la suma total del contenido de los ficheros
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
 		String[] vocales = { "A", "E", "I", "O", "U" };
 		String letra = null;
 		String nombreFicheroResultado = null;
-		String nombreFicheroEntrada = "vocales.txt"; //+ args[0];
+		String nombreFicheroEntrada = args[0];
 
 		for (int i = 0; i < vocales.length; i++) {
 			letra = vocales[i];
-			nombreFicheroResultado = PREFIJO_FICHEROS + String.valueOf(i+1);
+			nombreFicheroResultado = PREFIJO_FICHEROS + String.valueOf(i + 1);
 			lanzarSumadorVocales(nombreFicheroEntrada, letra, nombreFicheroResultado);
 		}
+		Thread.sleep(5000);
+		int sumaTotal = getSumaTotal(NUM_PROCESOS);
+		System.out.println("La suma total es:" + sumaTotal);
 	}
 }
--- Ejercicio8SYP/src/base/SumadorVocales.java
+++ Ejercicio8SYP/src/base/SumadorVocales.java
@@ -8,32 +8,41 @@ public class SumadorVocales {
 
 	/* Método para saber cuantas veces aparece una letra en el texto */
 	public static int leerLetras(String letra, String nombreFicheroEntrada) {
-
-		int valor = 0;
 		int contador = 0;
-		char caracter = letra.charAt(0);
+		int valor;
 		try {
-			FileReader fich = new FileReader(nombreFicheroEntrada);
+
+			FileReader fich = new FileReader(
+					"D:\\ProyectosEclipseSegundo\\proyectosEclipseClase\\Ejercicio8SYP\\" + nombreFicheroEntrada);
 			while ((valor = fich.read()) != -1) {
-				if (valor == caracter) {
-					contador++;
+
+				if ((String.valueOf((char) valor).toUpperCase()).equals(letra)) {
+					contador += 1;
 				}
 			}
+
 			fich.close();
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 		return contador;
 	}
 
+	/*
+	 * Método main por el que entran las argumentos y en el que invocamos el método
+	 * que sumara las vocales
+	 */
 	public static void main(String[] args) {
-
 		String nombreFicheroEntrada = args[0];
-		String letra = args[1];
+		String letra = (String) args[1];
 		int sumaVocales = leerLetras(letra, nombreFicheroEntrada);
 		System.out.println(sumaVocales);
+
 		System.out.flush();
 	}
 }
