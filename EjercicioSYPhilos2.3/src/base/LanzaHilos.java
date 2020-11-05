018117b9c6d8896c3fe7f376d98c62378b26265c
Barallobre <cristianbarallobrerua@gmail.com>
2020-11-05 09:57:30 +0100

    ejemplo hilo2

---
+++ EjercicioSYPhilos2.3/src/base/LanzaHilos.java
@@ -0,0 +1,13 @@
+package base;
+
+public class LanzaHilos {
+	public static void main(String[] args) {
+		int NUM_HILOS = 500;
+		EjecutorTareaCompleja op;
+		for (int i = 0; i < NUM_HILOS; i++) {
+			op = new EjecutorTareaCompleja("Operacion " + i);
+			Thread hilo = new Thread(op);
+			hilo.start();
+		}
+	}
+}
