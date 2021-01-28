bdb4a626a6d0ea1cbf6e3f69b50f43da44fc01ce
Barallobre <cristianbarallobrerua@gmail.com>
2021-01-28 10:10:37 +0100

    datagramas

---
+++ T2SYPEnvioRecepcionDatagramas2/src/base/UDPclienteEjemplo2.java
@@ -0,0 +1,45 @@
+package base;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class UDPclienteEjemplo2 {
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		
+		DatagramSocket clientSocket = new DatagramSocket();
+		
+		//DATOS DEL SERVIDOR AL QUE ENVIAR MENSAJE
+		InetAddress IPServidor = InetAddress.getLocalHost();
+		int puerto = 12345; //puerto por el que escucha
+		
+		//INTRODUCIR DATOS POR TECLADO
+		System.out.println("Introducce mensaje: ");
+		String cadena = sc.nextLine();
+		
+		byte[] enviados = new byte[1024];
+		enviados = cadena.getBytes(); //convertir cadena a bytes
+		
+		//ENVIANDO DATAGRAMA AL SERVIDOR
+		DatagramPacket envio = new DatagramPacket (enviados, enviados.length, IPServidor, puerto);
+		clientSocket.send(envio);
+		
+		//RECIBIENDO DATAGRAMA DEL SERVIDOR
+		byte[] recibidos = new byte[2];
+		DatagramPacket recibo = new DatagramPacket(recibidos, recibidos.length);
+		System.out.println("Esperando datagrama......");
+		clientSocket.receive(recibo);
+		
+		//Obtener el número de caracteres
+		byte[] hh = recibo.getData();
+		int numero = hh[0];
+		
+		System.out.println("Recibo Nº de caracteres que son a=> "+numero);
+		
+		clientSocket.close(); //cerrar socket
+	}
+
+}
---
+++ T2SYPEnvioRecepcionDatagramas2/src/base/UDPservidorEjemplo2.java
@@ -0,0 +1,50 @@
+package base;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class UDPservidorEjemplo2 {
+
+	public static void main(String[] args) throws IOException {
+		//ASOCIO EL SOCKET AL PUERTO 12345
+		DatagramSocket socket = new DatagramSocket(12345);
+		
+		//ESPERANDO DATAGRAM
+		System.out.println("Servidor esperando datagrama.......");
+		DatagramPacket recibo;
+		
+		byte[] bufer = new byte[1024];
+		recibo = new DatagramPacket(bufer, bufer.length);
+		socket.receive(recibo);//RECIBO DATAGRAMA
+		
+		String mensaje = new String(recibo.getData()).trim();
+		System.out.println("Servidor recibe: "+ mensaje);
+		
+		//CONTAR EL NÚMERO DE LETRAS a
+		int contador = 0;
+			for (int i = 0; i < mensaje.length(); i++) {
+				if(mensaje.charAt(i)=='a') {
+					contador++;
+				}
+			}
+			//DIRECCION ORIGEN DEL MENSAJE
+			InetAddress IPOrigen = recibo.getAddress();
+			int puerto = recibo.getPort();
+			
+			//ENVIANDO DATAGRAMA AL CLIENTE
+			System.out.println("Enviando número de apariciones de la letra a => "+contador);
+			byte b = (byte) contador; //paso entero a byte
+			byte [] enviados = new byte[1024];
+			enviados[0] = b;
+			
+			DatagramPacket envio = new DatagramPacket (enviados, enviados.length, IPOrigen, puerto);
+			socket.send(envio);
+			//CERRAR SOCKET
+			System.out.println("Cerrando conexión...");
+			socket.close();
+	}
+
+}
