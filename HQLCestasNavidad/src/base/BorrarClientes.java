3c8301c138a489e4d281149a0dbccee72eaa9391
Barallobre <cristianbarallobrerua@gmail.com>
2021-02-04 13:05:26 +0100

    crud

--- HQLCestasNavidad/src/base/BorrarClientes.java
+++ HQLCestasNavidad/src/base/BorrarClientes.java
@@ -1,33 +1,30 @@
 package base;
 
-import java.util.List;
-
-
-
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-import Primero.Clientes;
 import Primero.SessionFactoryUtil;
 
-public class ConsultaClientes {
+public class BorrarClientes {
 
 	public static void main(String[] args) {
 		SessionFactory sesionFactory = SessionFactoryUtil.obtenSessionFactory();
 		
 		Session sesion = null;
+		Transaction transaccion = null;
 		try {
 			sesion =sesionFactory.openSession();
-			
-			Query <Clientes> consulta = sesion.createQuery("from Clientes order by DNI asc", Clientes.class);
-			List <Clientes> lista = consulta.list();
-			for (int i=0;i<lista.size();i++) {
-				System.out.println(lista.get(i).getDni()+" - "+lista.get(i).getNombreRazonSocial());
-				
-			}
+			transaccion = sesion.beginTransaction();
+			Query SENTENCIA = sesion.createQuery("delete from FilmActor where actor < 7 ");
+			SENTENCIA.executeUpdate();
+			Query SENTENCIA2 = sesion.createQuery("delete from Actor where actorId < 7");
+			int numRegsBorrados = SENTENCIA2.executeUpdate();
+			System.out.println("Se han modificado: "+ numRegsBorrados +" registros.");
 		} catch (HibernateException e) {
+			transaccion.rollback();
 			System.out.println("Error");
 		}finally {
 			if (sesion!=null) {
@@ -36,5 +33,4 @@ public class ConsultaClientes {
 			}
 		}
 	}
-
 }
--- HQLCestasNavidad/src/base/ModificaClientes.java
+++ HQLCestasNavidad/src/base/ModificaClientes.java
@@ -1,5 +1,8 @@
 package base;
 
+
+
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -7,27 +10,28 @@ import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-import Primero.Clientes;
+
 import Primero.SessionFactoryUtil;
 
-public class ConsultaClientes {
+public class ModificaClientes {
 
 	public static void main(String[] args) {
 		SessionFactory sesionFactory = SessionFactoryUtil.obtenSessionFactory();
 		
 		Session sesion = null;
+		Transaction transaccion = null;
 		try {
 			sesion =sesionFactory.openSession();
-			
-			Query <Clientes> consulta = sesion.createQuery("from Clientes order by DNI asc", Clientes.class);
-			List <Clientes> lista = consulta.list();
-			for (int i=0;i<lista.size();i++) {
-				System.out.println(lista.get(i).getDni()+" - "+lista.get(i).getNombreRazonSocial());
-				
-			}
+			 transaccion = sesion.beginTransaction();
+			Query SENTENCIA = sesion.createQuery("update Clientes set Direccion = 'Coristanco' where DNI = '11111111'");
+			int numRegsModificados = SENTENCIA.executeUpdate();
+			System.out.println("Se han modificado: "+ numRegsModificados +" registros.");
+			transaccion.commit();
 		} catch (HibernateException e) {
+			transaccion.rollback();
 			System.out.println("Error");
 		}finally {
 			if (sesion!=null) {
