673087363642d01776f38cf5fca41edfff33d5ef
cristianbarallobrerua <cristianbarallobrerua@gmail.com>
2021-03-06 16:02:30 +0100

    fin parte principal

--- VehiculosAyuntamiento2021/src/base/Principal.java
+++ VehiculosAyuntamiento2021/src/base/Principal.java
@@ -3,11 +3,7 @@ package base;
 public class Principal {
 
 	public static void main(String[] args) {
-//		Altavehiculos frame = new Altavehiculos();
-//		frame.setVisible(true);
 		Barrademenu barramenu = new Barrademenu();
 		barramenu.setVisible(true);
-		//Escuchador e = new Escuchador(barramenu);
-		//barramenu.asignarEscuchador(e);
 	}
 }
